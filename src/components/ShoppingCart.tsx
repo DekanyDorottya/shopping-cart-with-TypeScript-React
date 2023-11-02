817d741e7e190c729459f886830807532c28d77e
Kanameyo <116904071+DekanyDorottya@users.noreply.github.com>
2023-11-02 17:38:56 +0100

    add localStorage to keep changes after refresh

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from 'react-bootstrap';
 import { useShoppingCart } from '../context/ShoppingCartContext';
 import { CartItem } from './CartItem';
+import { formatCurrency } from '../utilities/formatCurrency';
+import storeItems from '../data/items.json';
 
 type ShoppingCartProps = {
     isOpen: boolean;
@@ -14,7 +16,26 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
                 <Offcanvas.Title>Cart</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <Stack gap={3}>{cartItems.map(item=> (<CartItem key={item.id} {...item}/>))}</Stack>
+                <Stack gap={3}>
+                    {cartItems.map((item) => (
+                        <CartItem key={item.id} {...item} />
+                    ))}
+                    <div className='ms-auto fw-bold fs-5'>
+                        
+                        Total
+                        {formatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                                const item = storeItems.find(
+                                    (i) => i.id === cartItem.id
+                                );
+                                return (
+                                    total +
+                                    (item?.price || 0) * cartItem.quantity
+                                );
+                            },0)
+                        )}
+                    </div>
+                </Stack>
             </Offcanvas.Body>
         </Offcanvas>
     );
